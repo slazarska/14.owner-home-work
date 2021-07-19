f1356ad16f3eb40fe2537201d93adb6bccdf1f0e
Olga Slazarska <slazarska@protonmail.com>
2021-07-19 20:37:44 +0300

    added tests

--- src/test/java/tests/ApiTests.java
+++ src/test/java/tests/ApiTests.java
@@ -1,4 +1,16 @@
 package tests;
 
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+import tests.config.ApiConfig;
+
 public class ApiTests {
+
+    public static ApiConfig apiConfig = ConfigFactory.create(ApiConfig.class);
+
+    @Test
+    public void testApi(){
+        System.out.println("url: " + apiConfig.url());
+        System.out.println("token: " + apiConfig.token());
+    }
 }
--- src/test/java/tests/WebTests.java
+++ src/test/java/tests/WebTests.java
@@ -1,4 +1,22 @@
 package tests;
 
-public class WebTests {
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class WebTests extends TestBase {
+
+    @Test
+    public void openGitHub() {
+        System.out.println("browserName: " + webConfig.browserName());
+        System.out.println("browserVersion: " + webConfig.browserVersion());
+        System.out.println("remote: " + webConfig.remote());
+        System.out.println("selenideUrl: " + webConfig.selenideUrl());
+        System.out.println("selenideLogin: " + webConfig.selenideLogin());
+        System.out.println("selenidePassword: " + webConfig.selenidePassword());
+
+        open("https://github.com/");
+        sleep(5000);
+    }
 }
